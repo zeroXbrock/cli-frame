0173bf0c1092482c105c6afcb1521256f7d12531
zeroXbrock <2791467+zeroXbrock@users.noreply.github.com>
2024-04-19 11:13:54 -0700

    full frame

--- src/frame/mod.rs
+++ src/frame/mod.rs
@@ -4,7 +4,7 @@ use crossterm::{
 };
 use term_size;
 
-const FRAME_BG: &str = "░";
+const FRAME_BG: &str = "▓";
 const CLEAR: &str = " ";
 
 pub struct Frame {
@@ -84,24 +84,75 @@ impl Frame {
         (self.config.border_thickness + self.config.margin + self.config.padding) * 2
     }
 
+    fn left_padding(&self) -> String {
+        " ".repeat(self.config.padding)
+    }
+
+    fn left_margin(&self) -> String {
+        " ".repeat(self.config.margin)
+    }
+
+    fn frame_col(&self) -> String {
+        FRAME_BG.repeat(self.config.border_thickness)
+    }
+
+    fn frame_row(&self) -> String {
+        FRAME_BG.repeat(self.frame_width())
+    }
+
+    fn frame_width(&self) -> usize {
+        self.width - (self.config.margin * 2)
+    }
+
     /// Render the frame.
     fn render(&self) {
         if self.lines_buffer.len() > 0 {
-            if self.lines_buffer.len() > self.content_height() {
-                panic!("Frame content is too large to fit in frame.");
-            }
             reset_cursor();
-            let left_margin = " ".repeat(self.config.margin);
-            let frame = FRAME_BG.repeat(self.config.border_thickness);
-            let left_padding = " ".repeat(self.config.padding);
+
+            let top_margin = CLEAR.repeat(self.width);
+            // render top margin
+            for _ in 0..self.config.margin {
+                println!("{}", top_margin);
+            }
+
+            // render top border
+            let row_frame = format!("{}{}", self.left_margin(), self.frame_row());
+            let row_frame = (0..self.config.border_thickness)
+                .map(|_| row_frame.clone())
+                .collect::<Vec<String>>()
+                .join("\n");
+
+            // render top padding
+            let inner_padding_row = format!(
+                "{}{}{}{}",
+                self.left_margin(),
+                self.frame_col(),
+                CLEAR.repeat(self.content_width() + (self.config.padding * 2)),
+                self.frame_col(),
+            );
+
+            // render top border w/ padding, margins
+            println!("{}", row_frame);
+            println!("{}", inner_padding_row);
+
+            // render lines
             for line in self.lines_buffer.iter() {
                 let right_padding =
-                    " ".repeat(self.content_width() - line.len() + self.config.padding);
+                    CLEAR.repeat(self.content_width() - line.len() + self.config.padding);
                 println!(
                     "{}{}{}{}{}{}",
-                    left_margin, frame, left_padding, line, right_padding, frame,
+                    self.left_margin(),
+                    self.frame_col(),
+                    self.left_padding(),
+                    line,
+                    right_padding,
+                    self.frame_col(),
                 );
             }
+
+            // render bottom padding & border
+            println!("{}", inner_padding_row);
+            println!("{}", row_frame);
         }
     }
 
@@ -120,9 +171,8 @@ impl Frame {
     /// Update the frame by calling internal functions `clear` and `render`.
     pub fn update(&mut self, content: &str) {
         self.clear();
-        let lines = content.lines();
         let mut buf = vec![];
-        for line in lines {
+        for line in content.lines() {
             buf.extend(wrap_line(line, self.content_width()));
         }
         self.lines_buffer = buf.into_boxed_slice();
@@ -138,7 +188,7 @@ fn wrap_line(line: &str, width: usize) -> Vec<String> {
         lines.push(truncated.to_owned());
         lines.extend(wrap_line(&remainder, width));
     } else {
-        lines.push(line.trim().to_owned());
+        lines.push(line.to_owned());
     }
 
     lines
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,31 @@
 mod frame;
 
 use frame::FrameConfig;
+use resolve_path::PathResolveExt;
+use std::borrow::Cow;
+use std::{fs, path::Path};
 
 fn main() {
+    // read the file contents of frame/mod.rs
+    let test_file_path = match "src/main.rs".resolve() {
+        Cow::Borrowed(p) => p.canonicalize().expect("non canonical path"),
+        Cow::Owned(p) => p,
+    };
+    let contents = fs::read_to_string(test_file_path).expect("Failed to read file");
+
     // let mut frame = FrameConfig::default().frame();
     let mut frame = FrameConfig::new()
         .with_border_thickness(2)
         .with_padding(2)
         .with_margin(2)
         .frame();
-    frame.update("hey!\nthis is a multiline string\nwith 4 lines!\nlet me tell you something I'll tell you waht I've got half a mind to tell you what, you better lisen up boy I tell ya, you're in for it and I'm gonna give it to ya boy howdy I mean it buster I am so got dang serious mister");
+
+    frame.update("
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ac feugiat elit, quis molestie est. Aenean non sem sem. Donec auctor, ligula porttitor porttitor tempus, magna purus semper est, at aliquet dui ante at elit. Etiam maximus erat et nunc molestie, id aliquam ipsum vulputate. Maecenas justo lorem, convallis ac imperdiet sit amet, luctus quis quam. Fusce justo urna, maximus sed enim ut, euismod congue metus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer commodo iaculis arcu, vitae imperdiet magna porta ut. Integer tristique ipsum ut mauris semper vulputate. Pellentesque eget velit vel purus dictum auctor vel vitae enim.
+
+    Duis mi ex, scelerisque at urna vel, aliquet aliquet lorem. Vivamus at dapibus urna. Aenean vitae tempor est. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris at velit eu nunc venenatis gravida vitae sit amet est. Donec vitae scelerisque elit. Sed massa nisl, fermentum vel feugiat sit amet, scelerisque sed urna. Sed maximus vulputate libero a sagittis. Morbi eget nisl quis massa gravida accumsan vitae in nunc. Suspendisse viverra consectetur tortor eu sollicitudin. Sed convallis lacinia mi finibus dapibus. Donec imperdiet dui feugiat nisi porttitor ornare. Morbi posuere egestas pretium. Praesent id tincidunt augue. Mauris posuere bibendum vestibulum. Suspendisse in tristique tortor, quis volutpat lectus.
+    ");
     // sleep for a second
-    std::thread::sleep(std::time::Duration::from_secs(2));
-    frame.update("hey!\nthis is # multiline string\nwith 4 lines!\nlet me tell you something I'll tell you w#ht I've got h#lf # mind to tell you wh#t, you better lisen up boy I tell y#, you're in for it #nd I'm gonn# give it to y# boy howdy I mean it buster I am so got dang serious mister");
+    std::thread::sleep(std::time::Duration::from_secs(4));
+    frame.update(&contents);
 }
